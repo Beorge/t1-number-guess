b98a7a7016e2de8f2cfb5f0b06c1ca9cf88c588a
pyankovi <beorgerer64@gmail.com>
2024-06-13 14:24:39 +0500

    add extra info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getRandomInt } from "./utils";
+import { HINT_NAMES } from "./constants";
 
 // В этой задаче вам нужно создать компонент React, который генерирует случайное число между 1 и 100.
 // - Компонент должен позволять пользователю делать предположения о числе.
 // - Компонент должен отображать количество попыток пользователя.
+// - Компонент должен вывести подтверждение удачного угадывания.
 // - Компонент должен отображать сообщение, указывающее, является ли предположение пользователя выше или ниже, чем актуальное число.
-// - Компонент должен отображать список чисел введенных при попытках угадать число
+// - Компонент должен отображать список чисел введенных при попытках угадать число, список должен быть отсортирован по возрастанию
+//   в списке так же должна быть подсказка о том, что предположение пользователя больше или меньше, чем актуальное число,
+//   для подсказки использовать HINT_NAMES
 
 function App() {
+  const handleGuess = () => {};
+
+  const handleReset = () => {};
+
+  const handleInputChange = () => {};
   return (
     <div>
       <h1>Guess the Number!</h1>
-      <input type="number" value={0} />
+      <input onChange={handleInputChange} type="number" value={0} />
       {/* подсказка */}
       <p></p>
-      <button>Guess!</button>
-      <button>Reset</button>
+      <button onClick={handleGuess}>Guess!</button>
+      <button onClick={handleReset}>Reset</button>
       {/* число попыток */}
       <p>Attempts: </p>
       {/* список попыток */}
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const HINT_NAMES = {
+  id1: "Меньше",
+  id2: "Больше",
+};
