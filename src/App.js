7b27098c0046f39fb5f316a16438f3177fa4d517
Peregrinus <zakharov.mark22@gmail.com>
2024-07-24 19:40:28 +0300

    solve:problem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { getRandomInt } from "./utils";
-
+import { useState } from "react";
 // В этой задаче вам нужно создать компонент React, который генерирует случайное число между 1 и 100.
 // - Компонент должен позволять пользователю делать предположения о числе.
 // - Компонент должен отображать количество попыток пользователя.
@@ -8,18 +8,35 @@ import { getRandomInt } from "./utils";
 // - Компонент должен отображать список чисел введенных при попытках угадать число
 
 function App() {
+  const initRandom = () => getRandomInt(1, 100)
+  const [randomv,setRandomv] = useState(initRandom)
+  const [inputV, setInputV] = useState(0)
+  const [msg, setMsg] = useState('')
+  const [list, setList] = useState([])
   return (
     <div>
       <h1>Guess the Number!</h1>
-      <input type="number" value={0} />
+      <input type="number" value={inputV} onChange={(e)=>setInputV(Number(e.target.value))}/>
       {/* подсказка */}
       <p></p>
-      <button>Guess!</button>
-      <button>Reset</button>
+      <button onClick={()=>{
+        if(inputV > randomv) setMsg('Инпут больше');
+        else if(inputV === randomv) setMsg('Инпут равен');
+        else setMsg('Инпут меньше')
+
+        setList([...list, inputV])
+      }}>Guess!</button>
+      {msg}
+      <button onClick={()=>{
+        setMsg(''); 
+        setInputV(0)
+        setList([])
+        setRandomv(initRandom)
+      }}>Reset</button>
       {/* число попыток */}
       <p>Attempts: </p>
       {/* список попыток */}
-      <ul></ul>
+      <ul>{list.map(child=><li>{child}</li>)}</ul>
     </div>
   );
 }
